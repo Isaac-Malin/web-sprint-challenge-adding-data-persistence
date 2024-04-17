fbb4a95b08c3c219064db43bd4bfdb26eec2bd67
Isaac Malin <Isaacmalin3530@gmail.com>
2024-04-17 11:34:53 -0600

    getAll tasks function is working correctly

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,12 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+function getAll() {
+  return db('tasks as t')
+  .select('p.project_name', 'p.project_description', 't.*')
+  .leftJoin('projects as p', 't.project_id', 'p.project_id')
+}
+
+module.exports = {
+  getAll
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,19 @@
 // build your `/api/tasks` router here
+const router = require('express').Router()
+const Tasks = require('./model')
+
+router.get('/', async (req, res) => {
+  try {
+    const tasks = await Tasks.getAll()
+
+    const modifiedTasks = tasks.map(task => ({
+      ...task,
+      task_completed: task.task_completed ? true : false
+    }))
+    res.json(modifiedTasks)
+  } catch(err) {
+    res.status(404).json(err)
+  }
+})
+
+module.exports = router
